e971504b41862ed0556bc8120d908274e0b63714
Myles Pritchett <myles@unicard.com.au>
2025-02-04 13:31:33 +1100

    change to libsql from better-sqlite3

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,13 @@
 import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 
-const sqlite = new Database('sqlite.db');
-const db = drizzle(sqlite, { schema });
+const client = createClient({
+  url: "file:sqlite.db", // Path to your SQLite database file
+});
+const db = drizzle(client);
 
 // Export the Drizzle adapter for NextAuth.js
 export const authAdapter = DrizzleAdapter(db);
